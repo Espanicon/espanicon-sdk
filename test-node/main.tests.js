1aac4aa5dd72dca6d431d9148237b6f6f60e4923
FidelVe <43641727+FidelVe@users.noreply.github.com>
2022-07-31 16:39:35 -0400

    bumped version

--- test-node/main.tests.js
+++ test-node/main.tests.js
@@ -1,10 +1,8 @@
 // main.test.js
 const espaniconNodeSdk = require("./espanicon-node-sdk.test");
-const libWithSdkTests = require("./lib-with-sdk.test");
 const { runTestModule } = require("./utils");
 
 async function runAllTests() {
-  // await runTestModule(libWithSdkTests, "lib-with-sdk.js");
   await runTestModule(espaniconNodeSdk, "espanicon-node-sdk.js");
 }
 
